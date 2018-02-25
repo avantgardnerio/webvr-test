8b0af328c8f7385515d8dd104711734ce05e9a73
Brent Gardner <bgardner@squarelabs.net>
2018-02-25 18:04:11 +0100

    Canvas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-    console.log(`Hello console!`);
+    const cnvMain = document.createElement(`canvas`);
+    cnvMain.style.width = `100%`;
+    cnvMain.style.height = `100%`;
+    document.body.appendChild(cnvMain);
+
+    const onResize = () => {
+        cnvMain.width = cnvMain.offsetWidth * window.devicePixelRatio;
+        cnvMain.height = cnvMain.offsetHeight * window.devicePixelRatio;
+    };
+    onResize();
 };
