8529f29544b3a55bc8f0eea7c868d183fb827113
Brent Gardner <bgardner@squarelabs.net>
2018-03-04 20:25:57 +0100

    Can close shape

--- js/tools/RoomTool.mjs
+++ js/tools/RoomTool.mjs
@@ -5,39 +5,57 @@ export default class RoomTool {
         this.vertices = [];
         this.identity = mat4.create();
         this.currentController = undefined;
+        this.y = undefined;
     }
 
     onPress(gpIdx, btnIdx, positions) {
         this.currentController = gpIdx;
         const position = positions[gpIdx];
         console.log(`Adding `, position);
-        this.vertices.push(...position);
-        if(this.vertices.length === 1) this.vertices.push(...position);
+        this.vertices.push(this.snap(position[0]));
+        this.vertices.push(this.snap(position[1]));
+        this.vertices.push(this.snap(position[2]));
+        if (this.vertices.length === 3) {
+            this.vertices.push(this.snap(position[0]));
+            this.vertices.push(this.snap(position[1]));
+            this.vertices.push(this.snap(position[2]));
+            this.y = this.snap(position[1]);
+        } else if(
+            this.vertices[0] === this.vertices[this.vertices.length-3]
+            && this.vertices[1] === this.vertices[this.vertices.length-2]
+            && this.vertices[2] === this.vertices[this.vertices.length-1]
+        ) {
+            this.vertices = [];
+        }
         this.dirty = true;
     }
 
     onMove(positions) {
-        if(this.currentController === undefined) return;
-        if(this.vertices.length < 3) return;
-        this.vertices[this.vertices.length-3] = positions[this.currentController][0];
-        this.vertices[this.vertices.length-2] = positions[this.currentController][1];
-        this.vertices[this.vertices.length-1] = positions[this.currentController][2];
+        if (this.currentController === undefined) return;
+        if (this.vertices.length < 3) return;
+        this.vertices[this.vertices.length - 3] = this.snap(positions[this.currentController][0]);
+        this.vertices[this.vertices.length - 2] = this.y;
+        this.vertices[this.vertices.length - 1] = this.snap(positions[this.currentController][2]);
         this.dirty = true;
     }
-    
+
+    snap(coord) {
+        return Math.round(coord * 100) / 100;
+    }
+
     onRelease(gpIdx, btnIdx, positions) {
     }
 
     init(gl) {
-        if(!this.buffer) this.buffer = gl.createBuffer();
+        if (!this.buffer) this.buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
         this.dirty = false;
     }
 
     render(gl, shaderProgram) {
-        if(this.vertices.length < 3) return;
-        if(this.dirty) this.init(gl);
+        if (this.vertices.length < 3) return;
+        if (this.dirty) this.init(gl);
         gl.uniformMatrix4fv(shaderProgram.modelMat, false, this.identity);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
         gl.vertexAttribPointer(shaderProgram.vertPosAttr, 3, gl.FLOAT, false, 0, 0);
