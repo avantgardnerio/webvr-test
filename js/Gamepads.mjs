a4dc8f10e32c024eb1d07f81da57c3003faeecd1
Brent Gardner <bgardner@squarelabs.net>
2018-03-04 16:11:40 +0100

    Multiple gamepads

--- js/Gamepads.mjs
+++ js/Gamepads.mjs
@@ -31,21 +31,23 @@ export default class Gamepads {
     }
 
     render(gl, shaderProgram, vrDisplay) {
-        mat4.identity(this.myMatrix);
-        mat4.identity(this.gamepadMatHandle);
-        if (this.gamepads.length > 0) {
-            mat4.fromRotationTranslation(this.myMatrix, this.gamepads[0].pose.orientation, this.gamepads[0].pose.position);
-            mat4.multiply(this.myMatrix, vrDisplay.stageParameters.sittingToStandingTransform, this.myMatrix);
+        this.gamepads.forEach(gamepad => {
+            mat4.identity(this.myMatrix);
+            mat4.identity(this.gamepadMatHandle);
+            if (this.gamepads.length > 0) {
+                mat4.fromRotationTranslation(this.myMatrix, gamepad.pose.orientation, gamepad.pose.position);
+                mat4.multiply(this.myMatrix, vrDisplay.stageParameters.sittingToStandingTransform, this.myMatrix);
 
-            mat4.identity(this.gamepadMatTemp);
-            mat4.translate(this.gamepadMatTemp, this.gamepadMatTemp, [0, -0.5, -0.3]);
-            mat4.rotateX(this.gamepadMatTemp, this.gamepadMatTemp, -Math.PI * 0.2);
-            mat4.scale(this.gamepadMatTemp, this.gamepadMatTemp, [0.25, 0.25, 0.5]);
-            mat4.multiply(this.gamepadMatHandle, this.myMatrix, this.gamepadMatTemp);
-        }
-        gl.uniformMatrix4fv(shaderProgram.modelMat, false, this.gamepadMatHandle);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-        gl.vertexAttribPointer(shaderProgram.vertPosAttr, this.itemSize, gl.FLOAT, false, 0, 0);
-        gl.drawArrays(gl.LINE_STRIP, 0, this.numItems);
+                mat4.identity(this.gamepadMatTemp);
+                mat4.translate(this.gamepadMatTemp, this.gamepadMatTemp, [0, -0.5, -0.3]);
+                mat4.rotateX(this.gamepadMatTemp, this.gamepadMatTemp, -Math.PI * 0.2);
+                mat4.scale(this.gamepadMatTemp, this.gamepadMatTemp, [0.25, 0.25, 0.5]);
+                mat4.multiply(this.gamepadMatHandle, this.myMatrix, this.gamepadMatTemp);
+            }
+            gl.uniformMatrix4fv(shaderProgram.modelMat, false, this.gamepadMatHandle);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+            gl.vertexAttribPointer(shaderProgram.vertPosAttr, this.itemSize, gl.FLOAT, false, 0, 0);
+            gl.drawArrays(gl.LINE_STRIP, 0, this.numItems);
+        });
     }
 }
